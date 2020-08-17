ad63675cdf84c08b8c2d0c97ecae638d053dbe18
gittymutt <pedroschmitz@hotmail.com>
2020-08-16 21:27:52 -0400

    try to make automatic adding of text in fields, but it's not any more useful than cut and paste

--- browserAction/script.js
+++ browserAction/script.js
@@ -1,5 +1,9 @@
 
+var document.txtboxMsg;
+
 function listenForClicks() {
+  document.getElementsByClassName('textarea')[0].value = txtboxMsg;
+  console.log("testxmesage: " + txtboxMsg);
   document.addEventListener("click", (e) => {
 
     function makePresent(tabs) {
@@ -15,19 +19,33 @@ function listenForClicks() {
         });
     }
 
+    function fillIn(tabs) {
+        document.txtboxMsg = document.getElementsByClassName('textarea')[0].value;
+
+        browser.tabs.sendMessage(tabs[0].id, {
+          command: "fillIn",
+          message: document.txtboxMsg
+        });
+    }
+
     function reportError(error) {
       console.error(`Could not do it: ${error}`);
     }
+
 
     if (e.target.classList.contains("make-present")) {
       browser.tabs.query({active: true, currentWindow: true})
         .then(makePresent)
         .catch(reportError);
     }
-
     else if (e.target.classList.contains("campus-assign")) {
       browser.tabs.query({active: true, currentWindow: true})
         .then(campusAssign)
+        .catch(reportError);
+    }
+    else if (e.target.classList.contains("fill-in")) {
+      browser.tabs.query({active: true, currentWindow: true})
+        .then(fillIn)
         .catch(reportError);
     }
   });
--- content_script.js
+++ content_script.js
@@ -1,8 +1,10 @@
 (function() {
 
-
+  activeEl = document.activeElement;
   browser.runtime.onMessage.addListener((message) => {
-    console.log("the message: " + message);
+
+    
+
     if (message.command === "makePresent") {
       console.log("make it present!");
       Array.from(document.querySelectorAll('[name*="lstAttendAbsent"]')).map((el)=>{ if (!el.disabled) {el.selectedIndex=2}});
@@ -10,7 +12,8 @@
       console.log("assign campus");
       Array.from(document.querySelectorAll('[name*="dTutorialCampus"]')).map((el)=>{ if (!el.disabled) {el.selectedIndex=2}});
       Array.from(document.querySelectorAll('[name*="dAssign"]')).map((el)=>{ if (!el.disabled) {el.selectedIndex=2}})
+    } else if (message.command === "fillIn") {
+      activeEl.value = message.message;
     }
   });
-
 })();
